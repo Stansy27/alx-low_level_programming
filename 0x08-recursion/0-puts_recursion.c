cfa5a584b5e0364076515a3473c23c900eca80e5
Stansy27 <Stansy2727@yahoo.com>
2023-03-08 09:32:04 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _putchar_recursion - print a string followed by a new line
+ * @s: string
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
