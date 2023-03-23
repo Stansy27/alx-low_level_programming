e8cbe55006736a93eb949b456244a56639f7fd2c
Stansy27 <Stansy2727@yahoo.com>
2023-03-22 21:55:10 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,12 +12,12 @@ int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
 	op_t ops[] = {
-		{ "+", op_add },
-		{ "-", op_sub },
-		{ "*", op_mul },
-		{ "/", op_div },
-		{ "%", op_mod },
-		{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 
 	while (s && ops[i].op != NULL)
